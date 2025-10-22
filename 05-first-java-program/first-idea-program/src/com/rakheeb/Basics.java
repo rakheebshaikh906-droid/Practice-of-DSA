88f97b915138b4dd4e87eb62b075811b6a5a03f5
rakheebshaikh906-droid <rakheebshaikh906@gmail.com>
2025-10-22 18:06:20 +0530

    Add files via upload

---
+++ 05-first-java-program/first-idea-program/src/com/rakheeb/Basics.java
@@ -0,0 +1,25 @@
+package com.asim;
+
+public class Basics {
+    public static void main(String[] args) {
+//        if (true ){
+//            System.out.println("hello world");
+//        }
+//        int a = 50;
+//        if (a == 50){
+//                    System.out.println("hello world");
+//                }
+       // while loop
+//        int count = 1;
+//        while(count!=5){
+//            System.out.println(count);
+//            count++;
+//        }
+//
+       // for loop
+        for(int count = 1;count !=5;count++) {
+            System.out.println(count);
+        }
+
+    }
+}
