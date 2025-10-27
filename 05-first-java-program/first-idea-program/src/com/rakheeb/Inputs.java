6634bd608b276fbeaa6e441a81e4b797889a5989
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-10-27 14:44:47 +0530

    Add files via upload

---
+++ 05-first-java-program/first-idea-program/src/com/rakheeb/Inputs.java
@@ -0,0 +1,13 @@
+package com.asim;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your rollno : ");
+        int rollno = input.nextInt();
+        System.out.println("your rollno is : "+ rollno);
+
+    }
+}
