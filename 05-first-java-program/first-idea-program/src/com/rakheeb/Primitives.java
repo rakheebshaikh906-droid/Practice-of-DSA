3c6a7799d7dbf4a500a39b37bfdedb48dd2c49b0
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-10-27 14:44:14 +0530

    Add files via upload

---
+++ 05-first-java-program/first-idea-program/src/com/rakheeb/Primitives.java
@@ -0,0 +1,12 @@
+package com.asim;
+
+public class Primitives {
+    public static void main(String[] args) {
+//        int rollno = 62;
+//        char letter = 'a';
+//        float marks = 95.15f;
+//        double largeDecimalNumber = 6354.5343;
+//        long largeInteger = 123456789L;
+//        boolean check = true ;
+    }
+}
