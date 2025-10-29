347fc49d5c175d39a3a175621e4b5709c3ff8f38
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-10-29 13:48:31 +0530

    Add files via upload

---
+++ 05-first-java-program/first-idea-program/src/com/rakheeb/Temperate.java
@@ -0,0 +1,13 @@
+package com.asim;
+
+import java.util.Scanner;
+
+public class Temperate {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("please enter the temperate in C : ");
+        float tempC = in.nextFloat();
+        float tempF = (tempC * 9/5)+32 ;
+        System.out.println("the temp in fehranite is "+tempF);
+    }
+}
