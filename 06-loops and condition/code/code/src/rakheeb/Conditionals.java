4ba5b288bf648989d93eb0b554929c06bd3dfdfb
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-11-10 09:04:24 +0530

    Update Conditionals.java

--- 06-loops and condition/code/code/src/rakheeb/Conditionals.java
+++ 06-loops and condition/code/code/src/rakheeb/Conditionals.java
@@ -1,4 +1,32 @@
-package asim;
+package rakheeb;
 
 public class Conditionals {
+  public static void main(String[] args) {
+        int marks = 85;
+
+        // Simple if-else condition
+        if (marks >= 90) {
+            System.out.println("Excellent!");
+        } 
+        else if (marks >= 75) {
+            System.out.println("Very Good!");
+        } 
+        else if (marks >= 50) {
+            System.out.println("Good, but you can do better!");
+        } 
+        else {
+            System.out.println("Needs improvement");
+        }
+
+        // Nested if example
+        if (marks >= 60) {
+            System.out.println("You passed the exam!");
+            if (marks >= 80) {
+                System.out.println("You also got distinction 🎉");
+            }
+        } else {
+            System.out.println("Sorry, you failed the exam 😔");
+        }
+    }
 }
+
