144ff5b36568b3466576c8e279920192e1cbd29a
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-11-10 09:05:28 +0530

    Add files via upload

---
+++ 06-loops and condition/code/code/src/rakheeb/CountNums.java
@@ -0,0 +1,21 @@
+package asim;
+
+import java.util.Scanner;
+
+public class CountNums {
+    public static void main(String[] args) {
+        // print the number of 3's in n
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int count = 0;
+        while (n>0){
+            int rem = n%10;
+            if (rem ==3){
+                count++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
