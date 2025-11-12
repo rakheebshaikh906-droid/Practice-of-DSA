0efa55c1b6def88e6b689f7c91e9d4687c9933f5
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-11-12 14:31:16 +0530

    Add files via upload

---
+++ 06-loops and condition/code/code/src/rakheeb/Largest.java
@@ -0,0 +1,24 @@
+package asim;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int max = a;
+        if (b>max){
+             max=b;
+        }
+        if (c>max){
+            max=c;
+        }
+//        int max = Math.max(c,Math.max(a,b));
+//        // this line is equal to loop
+
+        System.out.println(max);
+    }
+}
