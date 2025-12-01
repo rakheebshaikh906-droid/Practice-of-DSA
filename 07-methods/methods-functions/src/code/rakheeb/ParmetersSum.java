c784b8b44f9c825c2dcbfaeba05669dd6ea3ae8e
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-12-01 15:58:04 +0530

    Add files via upload

---
+++ 07-methods/methods-functions/src/code/rakheeb/ParmetersSum.java
@@ -0,0 +1,18 @@
+package code.rakheeb;
+
+public class ParmetersSum {
+
+    // here we remove scannner thing and
+    // passing value at the time of calling
+    // functions or method
+
+    public static void main(String[] args) {
+        int ans = sum(20,30);
+        System.out.println(ans);
+    }
+    static int sum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+
+}
