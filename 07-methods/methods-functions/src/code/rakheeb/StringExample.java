223c7a6e3f1afcdb71319bc02624d141a29185eb
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2025-12-05 11:35:18 +0530

    Add files via upload

---
+++ 07-methods/methods-functions/src/code/rakheeb/StringExample.java
@@ -0,0 +1,13 @@
+package code.rakheeb;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet(){
+        String greet = "how are you ?";
+        return greet;
+    }
+}
