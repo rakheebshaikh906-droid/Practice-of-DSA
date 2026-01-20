9d0660e5ac0107e08d8c3cf1fbeb74c640a0bbd4
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-01-20 12:35:03 +0530

    Add files via upload

---
+++ 08-array/src/code/rakheeb/ArraysSort.java
@@ -0,0 +1,12 @@
+package code.rakheeb;
+
+import java.util.Arrays;
+
+//Q.sort the array using build in sort
+public class ArraysSort {
+    static void main(String[] args) {
+        int[]arr={30,43,54,65,42};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
