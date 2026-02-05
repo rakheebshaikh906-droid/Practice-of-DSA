7ae57818080b4814295cfa02e4005e14e94ca4db
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-02-05 13:28:08 +0530

    Add files via upload

---
+++ 08-array/src/code/rakheeb/Linearsearchin2D.java
@@ -0,0 +1,27 @@
+package code.rakheeb;
+
+import java.util.Arrays;
+
+//Q.linear search in 2D array
+public class Linearsearchin2D {
+    static void main(String[] args) {
+        int[][] arr={
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11}
+        };
+        int target=7;
+        int[]ans=search(arr,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int[][]arr,int target){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
