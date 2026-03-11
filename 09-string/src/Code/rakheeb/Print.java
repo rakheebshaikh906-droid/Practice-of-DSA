972016470c41bcda0647c893465ae4da44018048
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-03-12 02:27:27 +0530

    Add files via upload

---
+++ 09-string/src/Code/rakheeb/Print.java
@@ -0,0 +1,15 @@
+package Code.rakheeb;
+//simple print your name,favorite colour,and favorite footballer
+public class Print {
+    static void main(String[] args) {
+        // Declare and initialize string variables
+        String name="Shaikh abdul rakheeb";//stores your name
+        String colour="My favorite colour is black"; // stores your favorite colour
+        String Goat="My favorite footballer is Cristiano Ronaldo"; // stores your favorite footballer
+
+        // Print all the information on the console
+        System.out.println(name);
+        System.out.println(colour);
+        System.out.println(Goat);
+    }
+}
