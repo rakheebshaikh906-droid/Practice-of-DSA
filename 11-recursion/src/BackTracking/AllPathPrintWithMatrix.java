9ca6ea867fe525b89d892cbb70026db9fb5260dc
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-04-22 15:22:50 +0530

    Add files via upload

---
+++ 11-recursion/src/BackTracking/AllPathPrintWithMatrix.java
@@ -0,0 +1,49 @@
+package BackTracking;
+
+import java.util.Arrays;
+
+public class AllPathPrintWithMatrix {
+    static void main(String[] args) {
+        boolean[][] board={
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}
+        };
+        int[][]path=new int[board.length][board[0].length];
+        allPathPrint("",board,0,0,path,1);
+    }
+    static void allPathPrint(String p,boolean[][]maze,int r,int c,int[][]path,int step){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            path[r][c]=step;
+            for(int[]arr:path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+        //i am consider this block in my path
+        maze[r][c]=false;
+        path[r][c]=step;
+
+        if(r<maze.length-1){
+            allPathPrint(p+'D',maze,r+1,c,path,step+1);
+        }
+        if(c<maze[0].length-1){
+            allPathPrint(p+'R',maze,r,c+1,path,step+1);
+        }
+        if(r>0){
+            allPathPrint(p+'U',maze,r-1,c,path,step+1);
+        }
+        if(c>0){
+            allPathPrint(p+'L',maze,r,c-1,path,step+1);
+        }
+        //this line where the function will over
+        //so before the get function remove,also remove the changes that we are maded
+        maze[r][c]=true;
+        path[r][c]=0;
+    }
+}
