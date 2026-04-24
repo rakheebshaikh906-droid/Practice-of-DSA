c6f76feb51ddb5da89c9cb30c6049bc2334da1d3
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-04-24 15:58:09 +0530

    Add files via upload

---
+++ 11-recursion/src/BackTracking/NQueen.java
@@ -0,0 +1,64 @@
+package BackTracking;
+
+public class NQueen {
+    static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queens(board, 0));
+    }
+    static int queens(boolean[][]board,int row){
+        if(row==board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count=0;
+        //placing the queen and checking for every row and col
+        for(int col=0;col< board.length;col++){
+            //place the queen if its safe
+            if(isSafe(board,row,col)){
+                board[row][col]=true;
+                count+=queens(board,row+1);
+                board[row][col]=false;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][]board,int row,int col){
+        //check vertical row
+        for(int i=0;i<row;i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+        // left diagonal
+        for (int i = 1; row - i >= 0 && col - i >= 0; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        // right diagonal
+        for (int i = 1; row - i >= 0 && col + i < board.length; i++) {
+            if (board[row - i][col + i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private static void display(boolean[][] board){
+        for(boolean[] row: board){
+            for(boolean element : row){
+                if(element){
+                    System.out.print("Q");
+                }else{
+                    System.out.print("V");
+                }
+            }
+            //print a new line
+            System.out.println();
+        }
+    }
+}
