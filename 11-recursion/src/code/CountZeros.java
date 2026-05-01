b05392b54bdf4ae1020597bec2d440d9c54ec4a2
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-05-01 14:38:00 +0530

    Add files via upload

---
+++ 11-recursion/src/code/CountZeros.java
@@ -0,0 +1,21 @@
+package code;
+
+public class CountZeros {
+    static void main(String[] args) {
+        System.out.println(count(1006250));
+    }
+    static int count(int n){
+        return helper(n,0);
+    }
+    private static int helper(int n,int c){
+        if(n==0){
+            return c;
+        }
+        int rem=n%10;
+        if(rem==0){
+            return helper(n/10,c+1);
+        }else{
+            return helper(n/10,c);
+        }
+    }
+}
