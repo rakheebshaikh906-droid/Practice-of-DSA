f2677c7511d12a9c7f276b6553e55e209ce1d9e2
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-05-07 12:49:41 +0530

    Add files via upload

---
+++ 11-recursion/src/code/Sum.java
@@ -0,0 +1,14 @@
+package code;
+
+public class Sum {
+    static void main(String[] args) {
+        int ans=sum(10);
+        System.out.println(ans);
+    }
+    static int sum(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n+sum(n-1);
+    }
+}
