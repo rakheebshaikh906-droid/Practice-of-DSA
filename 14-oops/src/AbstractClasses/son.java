c054da5ae8fd844acf7a2a6aeae1e6ac58d0c54c
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-14 23:13:56 +0530

    Add files via upload

---
+++ 14-oops/src/AbstractClasses/son.java
@@ -0,0 +1,13 @@
+package AbstractClasses;
+
+public class son extends parent{
+    @Override
+    void career() {
+        System.out.println("I want to be a coder");
+    }
+
+    @Override
+    void partner() {
+        System.out.println("my partner is SA");
+    }
+}
