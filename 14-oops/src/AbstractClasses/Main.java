655c66960e9dbeea750d49c786125997d7571360
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-14 23:12:58 +0530

    Add files via upload

---
+++ 14-oops/src/AbstractClasses/Main.java
@@ -0,0 +1,11 @@
+package AbstractClasses;
+
+public class Main {
+    static void main(String[] args) {
+        son Son = new son();
+        Son.career();
+
+        daughter Daughter = new daughter();
+        Daughter.career();
+    }
+}
