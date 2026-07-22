84e3f513b325756a81fe752eec233561ff891241
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-22 12:35:12 +0530

    Add files via upload

---
+++ 14-oops/src/Properties/inheritance/MultipleInheritance.java
@@ -0,0 +1,8 @@
+package Properties.inheritance;
+//main class
+public class MultipleInheritance {
+    static void main(String[] args) {
+        //multilevel inheritance is a type of inheritance in which a single child class inherits property of more than one parents class
+        //java does not support multiple inheritance
+    }
+}
