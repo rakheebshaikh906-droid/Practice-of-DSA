008fe4b10a4f5e63ec7576a5d7f72cc4c9709a09
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-24 11:09:43 +0530

    Add files via upload

---
+++ 14-oops/src/Singleton/Test.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2); // true
+    }
+}
