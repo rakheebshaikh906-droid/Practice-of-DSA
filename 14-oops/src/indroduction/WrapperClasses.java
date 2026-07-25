cabca72badae9db95710ca6330d096780c24907a
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-25 11:04:26 +0530

    Add files via upload

---
+++ 14-oops/src/indroduction/WrapperClasses.java
@@ -0,0 +1,23 @@
+package indroduction;
+
+public class WrapperClasses {
+    static void main(String[] args) {
+        Integer i = 10;
+        Byte b = 5;
+        Short s = 15;
+        Long l = 1000L;
+        Float f = 10.5f;
+        Double d = 99.99;
+        Character c = 'A';
+        Boolean flag = true;
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+}
