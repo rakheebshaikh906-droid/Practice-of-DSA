68e0d03c1d2c28bf96358b8122ad816dc0576e4f
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-07-27 12:23:02 +0530

    Add files via upload

---
+++ 14-oops/src/interfaces/Main.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public class Main {
+    static void main(String[] args) {
+
+        Car car = new Car();
+
+        car.start();
+        car.stop();
+        car.acc();
+        car.brake();
+
+    }
+}
