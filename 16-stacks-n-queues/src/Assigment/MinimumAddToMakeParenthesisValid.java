d70c339bbb0bbdb0ba8d16db27bda86e8dc1a1bc
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-08-07 12:07:19 +0530

    Add files via upload

---
+++ 16-stacks-n-queues/src/Assigment/MinimumAddToMakeParenthesisValid.java
@@ -0,0 +1,29 @@
+package Assigment;
+
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+//921. Minimum Add to Make Parentheses Valid
+
+import java.util.Stack;
+
+public class MinimumAddToMakeParenthesisValid {
+    static void main(String[] args) {
+        String s="(((";
+        System.out.println(minAddToMakeValid(s));
+    }
+    static int minAddToMakeValid(String s) {
+        Stack<Character>stack=new Stack<>();
+
+        for(char ch:s.toCharArray()){
+            if(ch==')'){
+                if(!stack.isEmpty()&&stack.peek()=='('){
+                    stack.pop();
+                }else {
+                    stack.push(ch);
+                }
+            }else{
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}
