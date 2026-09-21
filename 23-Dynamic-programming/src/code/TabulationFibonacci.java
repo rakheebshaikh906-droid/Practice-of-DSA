8f3d0617d4f80bb79d2e7898a9d80ccfebc009d9
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-09-21 12:05:43 +0530

    Add files via upload

---
+++ 23-Dynamic-programming/src/code/TabulationFibonacci.java
@@ -0,0 +1,25 @@
+package code;
+
+//bottom-up dp
+public class TabulationFibonacci {
+    static void main(String[] args) {
+        int n=6;
+        System.out.println(fib(n));
+    }
+    static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        int[]dp=new int[n+1];
+
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+}
